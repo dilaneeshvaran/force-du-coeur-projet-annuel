d7b94624b36cdfb14c25b8abfad7a26f60fb0f92
EESHVARAN Dilan <ieeshvarand@gmail.com>
2024-04-15 17:04:41 +0200

    backoffice menu+icons
    Signed-off-by: EESHVARAN Dilan <ieeshvarand@gmail.com>

--- backoffice/src/App.tsx
+++ backoffice/src/App.tsx
@@ -1,34 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
+import Menu from './components/Menu';
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('isLoggedIn') === 'true');
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    localStorage.setItem('isLoggedIn', 'false');
+  };
+
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Menu className="navContainer" isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+      <Routes>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ backoffice/src/components/Menu.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import '../styles/menu.css'
+import fdcLogo from '../assets/fdc-logo.png'
+import CampaignIcon from '@mui/icons-material/Campaign';
+import ChecklistRtlIcon from '@mui/icons-material/ChecklistRtl';
+import Diversity1Icon from '@mui/icons-material/Diversity1';
+import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import SavingsIcon from '@mui/icons-material/Savings';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+import SpeedIcon from '@mui/icons-material/Speed';
+
+interface MenuProps {
+    className?: string;
+    isLoggedIn: boolean;
+    onLogout: () => void;
+}
+
+
+const Menu: React.FC<MenuProps> = ({ className, isLoggedIn, onLogout }) => {
+    const [isOpen, setOpen] = useState(false);
+    const location = useLocation();
+
+    return (
+        <nav
+            className={`navbar is-primary ${className}`}
+            role="navigation"
+            aria-label="main navigation"
+        >
+
+            <div className="container">
+                <div className="logo">
+                    <a href="https://github.com/dilaneeshvaran/force-du-coeur-projet-annuel" target="_blank">
+                        <img src={fdcLogo} className="logofdc" alt="fdc logo" />
+                    </a>
+                </div>
+                <NavLink className={`navbar-item ${location.pathname === '/home' ? 'is-active' : ''}`} to="/">
+                    <SpeedIcon />Performance
+                </NavLink>
+
+                <NavLink className={`navbar-item ${location.pathname === '/evenements' ? 'is-active' : ''}`} to="/evenements">
+                    <NewspaperIcon />Content Manage
+                </NavLink>
+
+                <NavLink className={`navbar-item ${location.pathname === '/missions' ? 'is-active' : ''}`} to="/missions">
+                    <Diversity1Icon /> Members
+                </NavLink>
+
+                <NavLink className={`navbar-item ${location.pathname === '/equipes' ? 'is-active' : ''}`} to="/equipes">
+                    <ChecklistRtlIcon />Tasks Manage
+                </NavLink>
+
+                <NavLink className={`navbar-item ${location.pathname === '/contact' ? 'is-active' : ''}`} to="/contact">
+                    <SavingsIcon />Finance
+                </NavLink>
+
+                <NavLink className={`navbar-item ${location.pathname === '/soutenir' ? 'is-active' : ''}`} to="/soutenir">
+                    <InventoryIcon />Inventory
+                </NavLink>
+                <NavLink className={`navbar-item ${location.pathname === '/rejoindre' ? 'is-active' : ''}`} to="/rejoindre">
+                    <MailOutlineIcon /> Messages
+                </NavLink>
+
+                <NavLink className={`navbar-item ${location.pathname === '/espaceMembres' ? 'is-active' : ''}`} to="/espaceMembres">
+                    <VerifiedUserIcon />Manage Users
+                </NavLink>
+                <NavLink className={`navbar-item ${location.pathname === '/espaceMembres' ? 'is-active' : ''}`} to="/espaceMembres">
+                    <CampaignIcon /> Alerts
+                </NavLink>
+            </div>
+        </nav>
+    );
+};
+
+export default Menu;
